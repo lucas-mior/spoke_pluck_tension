b24cb954f73a64fbf46ec9b29ca28313b2720be1
lucas-mior <lucas.mior@tutamail.com>
2025-07-17 17:55:49 -0300

    dont use PortAudio, use rtaudio (seems to have fixed input overflow

--- audio_to_fifo.c
+++ audio_to_fifo.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
-#include <portaudio.h>
-#include <stdint.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <stdlib.h>
 #include <signal.h>
 #include <stdatomic.h>
 #include <time.h>
+#include <rtaudio/rtaudio_c.h>
 
 typedef int16_t int16;
 
@@ -20,32 +18,25 @@ typedef int16_t int16;
 atomic_int overflow_count = 0;
 volatile sig_atomic_t running = 1;
 
-static int
-audio_callback(const void *inputBuffer,
-               void *outputBuffer,
-               unsigned long framesPerBuffer,
-               const PaStreamCallbackTimeInfo *timeInfo,
-               PaStreamCallbackFlags statusFlags,
-               void *userData) {
+int record_callback(void *outputBuffer, void *inputBuffer,
+                    unsigned int nFrames, double streamTime,
+                    unsigned int status, void *userData) {
     int *fifo = userData;
     const int16 *in = inputBuffer;
-    (void) outputBuffer;
-    (void) timeInfo;
 
-    if (statusFlags & paInputOverflow) {
+    if (status & RTAUDIO_STATUS_INPUT_OVERFLOW) {
         atomic_fetch_add(&overflow_count, 1);
     }
 
-    if (inputBuffer == NULL) {
+    if (!in) {
         int16 zero = 0;
-        for (unsigned long i = 0; i < framesPerBuffer; i++) {
+        for (unsigned int i = 0; i < nFrames; i++)
             write(*fifo, &zero, sizeof(zero));
-        }
     } else {
-        write(*fifo, in, framesPerBuffer * sizeof(*in));
+        write(*fifo, in, nFrames * sizeof(*in));
     }
 
-    return paContinue;
+    return 0;
 }
 
 void sigint_handler(int signum) {
@@ -53,56 +44,65 @@ void sigint_handler(int signum) {
 }
 
 int main(void) {
-    PaStream *stream;
-    PaError pa_error;
-    int fifo;
-    int total = 0;
-    int seconds = 0;
+    rtaudio_stream_parameters_t iParams;
+    rtaudio_stream_options_t options;
+    rtaudio_t io = NULL;
+    int fifo, total = 0, seconds = 0;
 
-    signal(SIGINT, sigint_handler);
+    /* signal(SIGINT, sigint_handler); */
 
     if ((fifo = open(FIFO_PATH, O_WRONLY)) < 0) {
         fprintf(stderr, "Error opening %s: %s.\n", FIFO_PATH, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    if ((pa_error = Pa_Initialize()) != paNoError) {
-        fprintf(stderr, "Error initializing PortAudio: %s.\n", Pa_GetErrorText(pa_error));
+    io = rtaudio_create(RTAUDIO_API_UNSPECIFIED);
+    if (!io) {
+        fprintf(stderr, "Error initializing RtAudio.\n");
         exit(EXIT_FAILURE);
     }
 
-    pa_error = Pa_OpenDefaultStream(&stream,
-                                    1, 0,
-                                    paInt16,
-                                    SAMPLE_RATE,
-                                    FRAMES_PER_BUFFER,
-                                    audio_callback,
-                                    &fifo);
-    if (pa_error != paNoError) {
-        fprintf(stderr, "Error opening PortAudio stream: %s.\n", Pa_GetErrorText(pa_error));
+    iParams.device_id    = rtaudio_get_default_input_device(io);
+    iParams.num_channels = 1;
+    iParams.first_channel = 0;
+
+    options.flags = 0;
+    options.num_buffers = 0;
+    options.priority = 0;
+    options.name[0] = '\0';
+
+    unsigned int buffer_frames = FRAMES_PER_BUFFER;
+    if (rtaudio_open_stream(io, NULL, &iParams,
+                             RTAUDIO_FORMAT_SINT16,
+                             SAMPLE_RATE, &buffer_frames,
+                             record_callback, &fifo,
+                             &options, NULL) != RTAUDIO_ERROR_NONE) {
+        fprintf(stderr, "Error opening RtAudio stream: %s\n", rtaudio_error(io));
         exit(EXIT_FAILURE);
     }
 
-    if ((pa_error = Pa_StartStream(stream)) != paNoError) {
-        fprintf(stderr, "Error starting PortAudio stream: %s.\n", Pa_GetErrorText(pa_error));
+    if (rtaudio_start_stream(io) != RTAUDIO_ERROR_NONE) {
+        fprintf(stderr, "Error starting RtAudio stream: %s\n", rtaudio_error(io));
         exit(EXIT_FAILURE);
     }
 
     printf("Streaming audio to FIFO... Press Ctrl+C to stop.\n");
 
+#define SLEEP 2
     while (running) {
-        sleep(1);
+        sleep(SLEEP);
         int count = atomic_exchange(&overflow_count, 0);
         total += count;
-        seconds += 1;
-        double average = (double) total / seconds;
-        printf("Input overflow count in last second: %d | average: %.2f/s\n", count, average);
+        seconds += SLEEP;
+        double average = (double)total / seconds;
+        printf("Input overflows in last %d seconds: %d | average: %.2f/s\n",
+               SLEEP, count, average);
     }
 
-    Pa_StopStream(stream);
-    Pa_CloseStream(stream);
-    Pa_Terminate();
+    rtaudio_stop_stream(io);
+    rtaudio_close_stream(io);
+    rtaudio_destroy(io);
     close(fifo);
 
-    exit(EXIT_SUCCESS);
+    return 0;
 }
