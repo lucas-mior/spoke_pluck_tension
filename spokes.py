477c6168589b6845f6db32c73204231c30a4624d
lucas-mior <lucas.mior@tutamail.com>
2025-07-28 16:48:30 -0300

    spokes.py: add kgf

--- spokes.py
+++ spokes.py
@@ -20,6 +20,10 @@ def frequency(tension, length=SPOKE_LENGTH):
     return np.int32(np.round(np.sqrt(tension / MU_STEEL_2mm) / (2*length)))
 
 
+def newton2kgf(t):
+    return t / 9.80665
+
+
 if __name__ == "__main__":
     tension_values = np.linspace(start=50, stop=2000, num=1200)
     lengths = [0.15, 0.18, 0.20, 0.22, 0.25]
@@ -27,18 +31,18 @@ if __name__ == "__main__":
     acceptable_tension_min = 900
     acceptable_tension_max = 1200
 
-    plt.figure(figsize=(8, 5))
+    fig, ax_n = plt.subplots(figsize=(8, 5))
 
     for length in lengths:
         freq_values = frequency(tension_values, length)
-        line, = plt.plot(tension_values, freq_values, label=f'{length:.2f} m')
+        line, = ax_n.plot(tension_values, freq_values, label=f'{length:.2f} m')
 
         color = line.get_color()
 
         fmin = frequency(acceptable_tension_min, length)
         fmax = frequency(acceptable_tension_max, length)
 
-        plt.fill_betweenx(
+        ax_n.fill_betweenx(
             [fmin, fmax],
             acceptable_tension_min,
             acceptable_tension_max,
@@ -46,10 +50,19 @@ if __name__ == "__main__":
             alpha=0.2
         )
 
-    plt.xlabel('Tension (N)')
-    plt.ylabel('Frequency (Hz)')
-    plt.title('Frequency vs. Tension for Different Spoke Lengths')
-    plt.legend(title='Spoke Length')
-    plt.grid(True)
-    plt.tight_layout()
+    ax_n.set_xlabel('Tension (N)')
+    ax_n.set_ylabel('Frequency (Hz)')
+    ax_n.set_title('Frequency vs. Tension for Different Spoke Lengths')
+    ax_n.grid(True)
+
+    ax_kgf = ax_n.secondary_xaxis(
+        location='bottom',
+        functions=(newton2kgf, lambda kgf: kgf * 9.80665)
+    )
+    ax_kgf.set_xlabel("Tension (kgf)")
+
+    ax_n.spines['bottom'].set_position(('outward', 30))
+
+    ax_n.legend(title='Spoke Length')
+    fig.tight_layout()
     plt.show()
